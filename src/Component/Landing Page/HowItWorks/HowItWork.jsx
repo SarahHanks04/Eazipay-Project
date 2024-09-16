7848aad56e253f402f697bbfbb247230a24e96d9
SarahHanks04 <sarahhanks04@gmail.com>
2024-09-17 00:13:11 +0100

    updated

--- src/Component/Landing Page/HowItWorks/HowItWork.jsx
+++ src/Component/Landing Page/HowItWorks/HowItWork.jsx
@@ -6,7 +6,7 @@ const HowItWork = () => {
   return (
     <section className="bg-white w-full min-h-screen md:flex-row md:justify-around">
       {/* Header */}
-      <header className="text-center my-5 mx-auto">
+      <header className="text-center mt-[5rem] mb-[2.5rem] mx-auto">
         <h1 className="text-[#11453B] text-2xl md:text-4xl font-bold">
           How Eazipay Works
         </h1>
@@ -16,9 +16,9 @@ const HowItWork = () => {
       </header>
 
       {/* Main Content */}
-      <main className="relative flex flex-col justify-around md:flex-row items-center mt-8 md:mt-0">
+      <main className="relative w-[984px] h-[682px] flex flex-col justify-around md:flex-row items-center md:mt-0 mx-auto">
         {/* Circle Background */}
-        <div className="relative">
+        <div className="relative mt-36">
           <div className="w-[577.15px] h-[577.15px] rounded-full bg-[#F0F7EB] flex justify-center items-center">
             <div className="w-[518.16px] h-[518.16px] rounded-full bg-[#D9EBCD] flex justify-center items-center">
               <div className="w-[450.9px] h-[450.9px] rounded-full bg-[#11453B]"></div>
@@ -29,17 +29,20 @@ const HowItWork = () => {
           <img
             src={Mobile}
             alt="Run Payroll Easily"
-            height={100}
+            width={347.61}
+            height={682}
             className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-80 md:w-96"
           />
         </div>
 
         {/* Steps Image */}
-        <div className="mt-12 md:mt-0 md:ml-12">
+        <div className="mt-[12rem] md:mt-0 md:ml-12">
           <img
             src={Steps}
             alt="How Eazipay Works"
-            className="w-full max-w-xs md:max-w-md"
+            width={421}
+            height={608}
+            className="h-auto max-w-xs md:max-w-md"
           />
         </div>
       </main>
