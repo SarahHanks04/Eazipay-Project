65a2968d4b97ff6721ee9bd720e633124b04b967
SarahHanks04 <sarahhanks04@gmail.com>
2024-09-16 23:39:26 +0100

    updated

--- src/Component/Landing Page/HowItWorks/HowItWork.jsx
+++ src/Component/Landing Page/HowItWorks/HowItWork.jsx
@@ -13,18 +13,28 @@ const HowItWork = () => {
       </header>
 
       <main className="image-container w-full flex justify-center mt-4 md:mt-0">
+      <div className="flex justify-center items-center bg-gray-50">
+      {/* Outer Circle */}
+      <div className="w-[577.15px] h-[577.15px] rounded-full border-[0.19px] bg-[#F0F7EB] flex justify-center items-center">
+        {/* Middle Circle */}
+        <div className="w-[518.16px] h-[518.16px] rounded-full border-[0.19px] bg-[#D9EBCD] flex justify-center items-center">
+          {/* Inner Circle */}
+          <div className="w-[450.9px] h-[450.9px] rounded-full border-[0.19px] bg-[#11453B]"></div>
+        </div>
+      </div>
+    </div>
         <div>
           <div>
             <img
               src={Mobile}
-              alt="Friendly Laundromat"
+              alt="Run Payroll Easily"
               className="w-full max-w-xs md:max-w-md rounded-lg shadow-lg"
             />
           </div>
           <div>
             <img
               src={Steps}
-              alt="Friendly Laundromat"
+              alt="How Eazipay Works"
               className="w-full max-w-xs md:max-w-md rounded-lg shadow-lg"
             />
           </div>
