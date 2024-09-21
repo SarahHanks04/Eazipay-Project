8db61132ca5a212a58c19d5937d4952614a229d3
SarahHanks04 <sarahhanks04@gmail.com>
2024-09-21 04:34:33 +0100

    updated

--- src/Component/Individual/BottomNavbarComponent.jsx
+++ src/Component/Individual/BottomNavbarComponent.jsx
@@ -1,36 +1,52 @@
+import React from "react";
 import { Link } from "react-router-dom";
+import HomeColored from "../../assets/HomeColored.svg";
+import User from "../../assets/User.svg";
+import Payroll from "../../assets/Payroll.svg";
+import Quickloan from "../../assets/Quickloan.svg";
+import Group from "../../assets/Group.png";
 
 const BottomNavbarComponent = () => {
   return (
     <nav className="bg-white fixed bottom-0 left-0 right-0 shadow-lg p-4 mt-[5rem]">
       <ul className="flex justify-around text-sm">
         <li>
-          <Link to="/" className="flex flex-col items-center">
-            <span>🏠</span>
+          <Link to="/home" className="flex flex-col items-center">
+            <span>
+              <img src={HomeColored} alt="Home" />
+            </span>
             Home
           </Link>
         </li>
         <li>
           <Link to="/employee" className="flex flex-col items-center">
-            <span>👨‍💼</span>
+            <span>
+              <img src={User} alt="useer" />
+            </span>
             Employee
           </Link>
         </li>
         <li>
           <Link to="/payroll" className="flex flex-col items-center">
-            <span>💼</span>
+            <span>
+              <img src={Payroll} alt="Payroll" />
+            </span>
             Payroll
           </Link>
         </li>
         <li>
           <Link to="/loans" className="flex flex-col items-center">
-            <span>💳</span>
+            <span>
+              <img src={Quickloan} alt="Loand" />
+            </span>
             Loans
           </Link>
         </li>
         <li>
           <Link to="/more" className="flex flex-col items-center">
-            <span>⋯</span>
+            <span>
+              <img src={Group} alt="More" />
+            </span>
             More
           </Link>
         </li>
