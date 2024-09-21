8ed9ecb8f3b25ed77a80ecea18e98ee2361c032c
SarahHanks04 <sarahhanks04@gmail.com>
2024-09-21 23:31:04 +0100

    updated

--- src/Component/Individual/BottomNavbarComponent.jsx
+++ src/Component/Individual/BottomNavbarComponent.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import HomeColored from "../../assets/HomeColored.svg";
+import { Link, NavLink } from "react-router-dom";
+import Home from "../../assets/Home.svg";
 import User from "../../assets/User.svg";
 import Payroll from "../../assets/Payroll.svg";
 import Quickloan from "../../assets/Quickloan.svg";
@@ -11,12 +11,33 @@ const BottomNavbarComponent = () => {
     <nav className="bg-white fixed bottom-0 left-0 right-0 shadow-lg p-4 mt-[5rem]">
       <ul className="flex justify-around text-sm">
         <li>
-          <Link to="/home" className="flex flex-col items-center">
+          {/* <Link to="/home" className="flex flex-col items-center">
             <span>
-              <img src={HomeColored} alt="Home" />
+              <img src={Home} alt="Home" />
             </span>
             Home
-          </Link>
+          </Link> */}
+          <NavLink
+            to="/individual"
+            className={({ isActive }) =>
+              isActive
+                ? "flex flex-col items-center text-[#11453B] font-bold whitespace-nowrap"
+                : "flex flex-col items-center text-[#8D8E8D] whitespace-nowrap"
+            }
+          >
+            <span>
+              <img
+                src={Home}
+                alt="Home"
+                className={({ isActive }) =>
+                  isActive
+                    ? "bg-[#11453B]" 
+                    : ""
+                }
+              />
+            </span>
+            Home
+          </NavLink>
         </li>
         <li>
           <Link to="/employee" className="flex flex-col items-center">
