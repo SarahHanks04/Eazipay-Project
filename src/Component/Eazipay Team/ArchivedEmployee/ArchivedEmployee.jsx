40cb7e216df52d89a706e6b20fc6505333b2a1ee
SarahHanks04 <sarahhanks04@gmail.com>
2024-09-25 01:23:08 +0100

    updated

--- src/Component/Eazipay Team/ArchivedEmployee/ArchivedEmployee.jsx
+++ src/Component/Eazipay Team/ArchivedEmployee/ArchivedEmployee.jsx
@@ -1,39 +1,10 @@
-// import React from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { toggleView } from './teamSlice';
-
-// const ViewToggle = () => {
-//   const dispatch = useDispatch();
-//   const isDepartmentView = useSelector((state) => state.team.isDepartmentView);
-
-//   const handleToggle = () => {
-//     dispatch(toggleView());
-//   };
-
-//   return (
-//     <div className="toggle-buttons">
-//       <button 
-//         onClick={handleToggle} 
-//         className={!isDepartmentView ? "active" : ""}
-//       >
-//         All Employees
-//       </button>
-//       <button 
-//         onClick={handleToggle} 
-//         className={isDepartmentView ? "active" : ""}
-//       >
-//         Departments
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default ViewToggle;
 import React from "react";
 import { useSelector } from "react-redux";
 
 const ArchivedEmployee = () => {
-  const archivedEmployees = useSelector((state) => state.team.archivedEmployees);
+  const archivedEmployees = useSelector(
+    (state) => state.team.archivedEmployees
+  );
 
   return (
     <div className="archived-list">
