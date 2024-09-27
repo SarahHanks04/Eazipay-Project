fb3abf4f3b9c5991c585afe79328fcfa15bf2870
SarahHanks04 <sarahhanks04@gmail.com>
2024-09-28 00:01:14 +0100

    updated

---
+++ src/Component/Settings/CACUpload.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setCACDocument } from '../Eazipay Team/Slice/Slice';
+
+const CACUpload = () => {
+  const dispatch = useDispatch();
+  const cacDocument = useSelector((state) => state.form.cacDocument);
+
+  const handleCACChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      dispatch(setCACDocument(file));
+    }
+  };
+
+  return (
+    <div>
+      <label htmlFor="cacUpload">Upload CAC Doc</label>
+      <input type="file" id="cacUpload" onChange={handleCACChange} />
+      {cacDocument && <p>{cacDocument.name}</p>}
+    </div>
+  );
+};
+
+export default CACUpload;
