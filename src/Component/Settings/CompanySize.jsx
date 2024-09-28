f2b1eca0c7abd4b289a3abfdcd31497968ec95e2
SarahHanks04 <sarahhanks04@gmail.com>
2024-09-28 02:29:08 +0100

    updated

--- src/Component/Settings/CompanySize.jsx
+++ src/Component/Settings/CompanySize.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { setCompanySize, setPensionCode } from "./Slice";
+import {
+  setCompanySize,
+  setIndustry,
+  setNhfCode,
+  setPayeId,
+  setPayeState,
+  setPensionCode,
+  setWebsite,
+} from "./Slice";
 
 const CompanySize = () => {
   const dispatch = useDispatch();
   const companySize = useSelector((state) => state.form.companySize);
   const pensionCode = useSelector((state) => state.form.pensionCode);
-  const pensionCode = useSelector((state) => state.form.pensionCode);
+  const payeState = useSelector((state) => state.form.payeState);
+  const payeId = useSelector((state) => state.form.payeId);
+  const nhfCode = useSelector((state) => state.form.nhfCode);
+  const website = useSelector((state) => state.form.website);
+  const industry = useSelector((state) => state.form.industry);
 
   return (
     <form className="relative mb-4">
@@ -23,15 +35,21 @@ const CompanySize = () => {
         value={companySize}
         onChange={(e) => dispatch(setCompanySize(e.target.value))}
       >
-        <option value="">Select size</option>
-        <option value="1-10">1-10</option>
-        <option value="11-50">11-50</option>
-        <option value="51-100">51-100</option>
         <option value="100 and above">100 and above</option>
+        <option value="81-99">81-99</option>
+        <option value="61-80">61-80</option>
+        <option value="41-60">41-60</option>
+        <option value="21-40">21-40</option>
+        <option value="1-20">1-20</option>
       </select>
-                {/* Pension Code */}
+      {/* Pension Code */}
       <div className="relative mb-4">
-        <label htmlFor="pensionCode" className="absolute -top-2 left-3 bg-white px-1 text-sm text-green-600">Pension Code</label>
+        <label
+          htmlFor="pensionCode"
+          className="absolute -top-2 left-3 bg-white px-1 text-sm text-green-600"
+        >
+          Pension Code
+        </label>
         <input
           type="text"
           id="pensionCode"
@@ -43,23 +61,95 @@ const CompanySize = () => {
 
       {/* PAYE State */}
       <label
-        htmlFor="companySize"
+        htmlFor="payeState"
         className="absolute -top-2 left-3 bg-white px-1 text-sm text-green-600"
       >
-        Company Size
+        PAYE State
       </label>
       <select
-        id="companySize"
+        id="payeState"
         className="block w-full px-4 py-2 border border-green-600 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
-        value={companySize}
-        onChange={(e) => dispatch(setCompanySize(e.target.value))}
+        value={payeState}
+        onChange={(e) => dispatch(setPayeState(e.target.value))}
       >
-        <option value="">Select size</option>
-        <option value="1-10">1-10</option>
-        <option value="11-50">11-50</option>
-        <option value="51-100">51-100</option>
-        <option value="100 and above">100 and above</option>
+        <option value="">Lagos</option>
+        <option value="abuja">Abuja</option>
+        <option value="portHarcort">Port Harcort</option>
+        <option value="enugu">Enugu</option>
+        <option value="kano">Kano</option>
+        <option value="ogunState">Ogun State</option>
+        <option value="kogiState">Kogi State</option>
+        <option value="edoState">Edo State</option>
+        <option value="calabar">Calabar</option>
       </select>
+
+      {/* PAYE ID */}
+      <div className="relative mb-4">
+        <label
+          htmlFor="payeId"
+          className="absolute -top-2 left-3 bg-white px-1 text-sm text-green-600"
+        >
+          PAYE ID
+        </label>
+        <input
+          type="text"
+          id="payeId"
+          className="block w-full px-4 py-2 border border-green-600 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
+          value={payeId}
+          onChange={(e) => dispatch(setPayeId(e.target.value))}
+        />
+      </div>
+
+      {/* NHF CODE */}
+      <div className="relative mb-4">
+        <label
+          htmlFor="nhfCode"
+          className="absolute -top-2 left-3 bg-white px-1 text-sm text-green-600"
+        >
+          NHF Code
+        </label>
+        <input
+          type="text"
+          id="nhfCode"
+          className="block w-full px-4 py-2 border border-green-600 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
+          value={nhfCode}
+          onChange={(e) => dispatch(setNhfCode(e.target.value))}
+        />
+      </div>
+
+      {/* WEBSITE */}
+      <div className="relative mb-4">
+        <label
+          htmlFor="website"
+          className="absolute -top-2 left-3 bg-white px-1 text-sm text-green-600"
+        >
+          Website
+        </label>
+        <input
+          type="text"
+          id="website"
+          className="block w-full px-4 py-2 border border-green-600 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
+          value={website}
+          onChange={(e) => dispatch(setWebsite(e.target.value))}
+        />
+      </div>
+
+      {/* INDUSTRY */}
+      <div className="relative mb-4">
+        <label
+          htmlFor="industry"
+          className="absolute -top-2 left-3 bg-white px-1 text-sm text-green-600"
+        >
+          Industry
+        </label>
+        <input
+          type="text"
+          id="industry"
+          className="block w-full px-4 py-2 border border-green-600 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
+          value={industry}
+          onChange={(e) => dispatch(setIndustry(e.target.value))}
+        />
+      </div>
     </form>
   );
 };
