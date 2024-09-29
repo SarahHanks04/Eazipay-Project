ed029de0b0f63bbd844bd4d82f789e61d4315b74
SarahHanks04 <sarahhanks04@gmail.com>
2024-09-30 00:01:25 +0100

    updated

--- src/Component/Settings/CompanyAddress.jsx
+++ src/Component/Settings/CompanyAddress.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { setIsRegistered, setOfficeAddress } from "./Slice";
+import { setOfficeAddress } from "./Slice";
 
 const CompanyAddress = () => {
   const dispatch = useDispatch();
-
   const officeAddress = useSelector((state) => state.form.officeAddress);
 
   const handleOfficeChange = (e) => {
@@ -15,29 +14,43 @@ const CompanyAddress = () => {
   };
 
   return (
-    <main>
+    <main className="px-4 sm:px-8">
       {/* COMPANY ADDRESS */}
-      <div className="relative mb-4">
+      <div className="relative my-4 sm:my-8">
         <label
-          htmlFor="industry"
-          className="absolute -top-2 left-3 bg-white px-1 text-sm text-[#8D8E8D]"
+          htmlFor="address"
+          className="absolute -top-3 left-4 sm:left-5 bg-white px-[8px] sm:px-[16px] text-[14px] sm:text-[16px] text-[#8D8E8D]"
         >
           Office Address
         </label>
         <input
           type="text"
           id="officeAddress"
-          className="block w-full px-4 py-2 border border-[#B5B6B5] rounded-md focus:outline-none focus:ring-1 focus:ring-[#11453B]"
+          className="block w-full px-3 py-2 sm:px-4 sm:py-2 border-[1.3px] border-[#B5B6B5] rounded-[12px] sm:rounded-[16px] focus:outline-none focus:ring-1 focus:ring-[#11453B]"
           value={officeAddress}
           onChange={(e) => dispatch(setOfficeAddress(e.target.value))}
         />
       </div>
-      {/* Proof Of Adress */}
-      <div>
-        <label htmlFor="cacUpload">
-          Upload Proof of Adress <span>e.g Nepa Bill, etc.</span>
-        </label>
-        <input type="file" id="cacUpload" onChange={handleOfficeChange} />
+
+      {/* Proof Of Address */}
+      <div className="relative flex items-center flex-wrap sm:flex-nowrap gap-3 my-[2rem] sm:my-[3rem]">
+        <div>
+          <input
+            type="file"
+            id="uploadAddress"
+            className="absolute inset-0 opacity-0 cursor-pointer"
+            onChange={handleOfficeChange}
+          />
+          <label
+            htmlFor="uploadAddress"
+            className="bg-[#F0F7EB] border-[1.5px] border-[#11453B] px-4 py-2 rounded-[8px] cursor-pointer shadow-sm text-[#11453B] hover:bg-[#4E4E4E] hover:text-white transition"
+          >
+            Upload Proof of Address
+          </label>
+        </div>
+        <span className="text-[12px] sm:text-[13px] text-[#4E4E4E]">
+          e.g Nepa Bill, etc
+        </span>
         {officeAddress && <p>{officeAddress.name}</p>}
       </div>
     </main>
--- src/Component/Settings/CompanyName.jsx
+++ src/Component/Settings/CompanyName.jsx
@@ -1,14 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { setCACDocument, setCompanyName, setIsRegistered } from "./Slice";
+import { setCACDocument, setCompanyName } from "./Slice";
 
 const CompanyName = () => {
   const dispatch = useDispatch();
   const companyName = useSelector((state) => state.form.companyName);
-  const isRegistered = useSelector((state) => state.form.isRegistered);
   const cacDocument = useSelector((state) => state.form.cacDocument);
-
-  const [fileName, setFileName] = useState("Upload CAC Doc.");
 
   const handleCACChange = (e) => {
     const file = e.target.files[0];
@@ -19,19 +16,19 @@ const CompanyName = () => {
   };
 
   return (
-    <main className="px-8">
+    <main className="px-4 sm:px-8">
       {/* COMPANY NAME */}
-      <div className="relative my-8">
+      <div className="relative my-4 sm:my-8">
         <label
           htmlFor="companyName"
-          className="absolute -top-3 left-5 bg-white px-[16px] text-[16px] text-[#8D8E8D]"
+          className="absolute -top-3 left-4 sm:left-5 bg-white px-[8px] sm:px-[16px] text-[14px] sm:text-[16px] text-[#8D8E8D]"
         >
           Company Name
         </label>
         <input
           type="text"
           id="companyName"
-          className="block w-full px-4 py-2 border-[1.3px] border-[#B5B6B5] rounded-[16px] focus:outline-none focus:ring-1 focus:ring-[#11453B]"
+          className="block w-full px-3 py-2 sm:px-4 sm:py-2 border-[1.3px] border-[#B5B6B5] rounded-[12px] sm:rounded-[16px] focus:outline-none focus:ring-1 focus:ring-[#11453B]"
           value={companyName}
           onChange={(e) => dispatch(setCompanyName(e.target.value))}
         />
@@ -41,26 +38,19 @@ const CompanyName = () => {
       <div>
         <label className="text-[#515251] text-[16px] font-[400]">
           Is your company registered?
-        </label>{" "}
-        <br />
-        <div className="mt-[1rem]">
-          <button
-            onClick={() => dispatch(setIsRegistered(true))}
-            className={isRegistered === true ? "active" : ""}
-          >
+        </label>
+        <div className="mt-4 sm:mt-[1rem] flex gap-4 sm:gap-8">
+          <button className="flex-1 py-2 rounded-md text-white bg-[#11453B] hover:bg-white hover:text-[#4E4E4E] transition-all">
             Yes
           </button>
-          <button
-            onClick={() => dispatch(setIsRegistered(false))}
-            className={isRegistered === false ? "active" : ""}
-          >
+          <button className="flex-1 py-2 rounded-md border-[2px] text-[#4E4E4E] border-[#F0F7EB] hover:bg-[#11453B] hover:text-white transition-all">
             No
           </button>
         </div>
       </div>
 
       {/* CAC DOCUMENT */}
-      <div className="relative flex items-baseline gap-3 my-[3rem]">
+      <div className="relative flex flex-wrap sm:flex-nowrap items-center gap-2 my-[2rem] sm:my-[3rem]">
         <div>
           <input
             type="file"
@@ -70,12 +60,14 @@ const CompanyName = () => {
           />
           <label
             htmlFor="cacDocument"
-            className="bg-[#F0F7EB] border-[1.5px] border-[#11453B] px-4 py-2 rounded-[8px] cursor-pointer shadow-sm text-[#11453B] hover:bg-gray-100 transition"
+            className="bg-[#F0F7EB] border-[1.5px] border-[#11453B] px-4 py-2 rounded-[8px] cursor-pointer shadow-sm text-[#11453B] hover:bg-[#4E4E4E] hover:text-white transition"
           >
-            {fileName}
+            Upload CAC Doc.
           </label>
         </div>
-        <span className="text-[13px] text-[#4E4E4E] items-baseline">Jpeg and Png Only (2mb max)</span>
+        <span className="text-[12px] sm:text-[13px] text-[#4E4E4E]">
+          Jpeg and Png Only (2mb max)
+        </span>
         {cacDocument && <p>{cacDocument.name}</p>}
       </div>
     </main>
