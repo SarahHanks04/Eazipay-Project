8e8057dadb8d8211fba220e565a114f59df9e1a1
SarahHanks04 <sarahhanks04@gmail.com>
2024-10-01 00:15:24 +0100

    Buttons Updated

--- src/Component/Settings/SettingButtons.jsx
+++ src/Component/Settings/SettingButtons.jsx
@@ -14,18 +14,20 @@ const SettingButtons = () => {
   // Handle Save Button
   const handleSave = () => {
     console.log("Saved Data:", formData);
-    // dispatch(resetForm());
+    dispatch(resetForm());
   };
 
   return (
     <div className="flex justify-between w-full space-x-4 mt-4 mb-8 px-5 sm:px-8">
       <button
+        type="button"
         onClick={handleCancel}
         className="bg-white border-[1.7px] border-[#11453B] text-[#11453B] py-3 px-4 w-full rounded-full hover:bg-[#11453B] hover:text-white transition"
       >
         Cancel
       </button>
       <button
+        type="button"
         onClick={handleSave}
         className="bg-white border-[1.7px] border-[#11453B] text-[#11453B] py-3 px-4 w-full rounded-full hover:bg-[#11453B] hover:text-white transition"
       >
