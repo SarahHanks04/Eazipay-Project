a280abab2e9cc6b48ae9e0d6704a201507b73e44
SarahHanks04 <sarahhanks04@gmail.com>
2024-10-05 00:39:34 +0100

    Sign Up Updated

--- src/Component/Login Page/LoginPage.jsx
+++ src/Component/Login Page/LoginPage.jsx
@@ -4,7 +4,7 @@ import { IndividualContext } from "../Individual/IndividualProvider";
 
 const LoginPage = () => {
   const { login, error, user } = useContext(IndividualContext);
-  const [username, setUsername] = useState(""); 
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
@@ -17,15 +17,15 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-[#DAE9D0]">
       <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md">
-        <h1 className="text-2xl font-semibold text-center text-gray-700 mb-4">
+        <h1 className="text-2xl font-semibold text-center text-[#515251] mb-4">
           Login
         </h1>
-        {error && <p className="text-red-500 text-center mb-2">{error}</p>}
+
         <form onSubmit={handleSubmit} className="space-y-4">
           <div className="flex flex-col">
-            <label className="text-gray-600 font-medium mb-1">Username</label>
+            <label className="text-[#515251] font-medium mb-1">Username</label>
             <input
               type="text"
               className="px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
@@ -35,7 +35,7 @@ const LoginPage = () => {
             />
           </div>
           <div className="flex flex-col">
-            <label className="text-gray-600 font-medium mb-1">Password</label>
+            <label className="text-[#515251] font-medium mb-1">Password</label>
             <input
               type="password"
               className="px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
@@ -44,16 +44,17 @@ const LoginPage = () => {
               required
             />
           </div>
+          {error && <p className="text-red-500 text-center mb-2 text-sm">{error}</p>}
           <button
             type="submit"
-            className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition-colors duration-300"
+            className="w-full bg-[#11453B] text-white py-2 rounded-lg hover:bg-[#DAE9D0] hover:text-[#515251] transition-colors duration-300"
           >
             Login
           </button>
         </form>
-        <p className="text-gray-500 text-center mt-4">
+        <p className="text-[#515251] text-center mt-4">
           Don't have an account?{" "}
-          <span className="text-blue-500 cursor-pointer">Sign up</span>
+          <span className="text-[#EA4E4B] cursor-pointer"> Register</span>
         </p>
       </div>
     </div>
