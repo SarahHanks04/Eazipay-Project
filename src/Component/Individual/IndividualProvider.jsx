26cfde6c7304159fa44526f57d414182c70afe98
SarahHanks04 <sarahhanks04@gmail.com>
2024-10-04 22:27:07 +0100

    updated

--- src/Component/Individual/IndividualProvider.jsx
+++ src/Component/Individual/IndividualProvider.jsx
@@ -4,15 +4,43 @@ export const IndividualContext = createContext();
 
 const IndividualProvider = ({ children }) => {
   const [balance, setBalance] = useState(false);
+  const [user, setUser] = useState("")
+  const [error, setError] = useState(null); // Error state
 
+  // Toggle Balance Visibility
   const toggleBalance = () => {
     setBalance((prev) => !prev);
   };
+
+  const login = async (username, password) => {
+    try {
+      const response = await axios.post("https://dummyjson.com/auth/login", {
+        username,
+        password,
+      });
+      setUser(response.data); // Set the user data returned by the API
+      setError(null); // Clear any previous errors
+    } catch (err) {
+      console.error("Login failed", err);
+      setError("Invalid username or password");
+    }
+  };
+
+
+  // Logout function
+  const logout = () => {
+    setUser(""); // Clear the user state to simulate logout
+  };
+
 
   const values = {
     balance,
     setBalance,
     toggleBalance,
+    user,
+    login,
+    logout,
+    error,
   };
   return (
     <IndividualContext.Provider value={values}>
